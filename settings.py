6007a788e82d154c5e0288428457ac2fdaa104fc
Tomek <tomek.owienko@gmail.com>
2021-12-22 21:44:42 +0100

    Updated to a correct implementation of the composite pattern, reworked the tile storing mechanism, implemented Tile class, implemented block generation, implemented basic keypress event handling, added docstrings

--- settings.py
+++ settings.py
@@ -1,4 +1,27 @@
+# TODO use json
 class Settings:
+    """
+    Class containing game settings
+    """
     def __init__(self):
-        self.REFRESH_RATE = 30  # hz
-        self.BOARD_SIZE = (10, 20)  # x, y
+        self._REFRESH_RATE = 30  # hz
+        self._BOARD_SIZE = (10, 20)  # x, y
+        self._WINDOW_SIZE = self._BOARD_SIZE
+        self._BLOCK_UPDATE_PERIOD = 0.4  # s
+
+    @property
+    def REFRESH_RATE(self):
+        return self._REFRESH_RATE
+
+    @property
+    def BOARD_SIZE(self):
+        return self._BOARD_SIZE
+
+    @property
+    def WINDOW_SIZE(self):
+        return self._WINDOW_SIZE
+
+    @property
+    def BLOCK_UPDATE_PERIOD(self):
+        return self._BLOCK_UPDATE_PERIOD
+
